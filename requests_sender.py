9552a0071d3e0f7381e1cca1ee97d2802eeb5e34
glebzok <mezentcev.g@outlook.com>
2020-10-20 17:30:05 +0300

    add: Procfile

--- requests_sender.py
+++ requests_sender.py
@@ -5,7 +5,7 @@ address = 'http://127.0.0.1:8000/'
 address1 = 'http://127.0.0.1:8000/search_by_query'
 #address2 = 'http://127.0.0.1:7000/search_by_datasets'
 address3 = 'https://damp-meadow-03187.herokuapp.com/hello'
-address4 = 'https://localhost:8000/hello'
+address4 = 'https://localhost:8080/hello'
 
 if __name__ == '__main__':
 
