2e889ef2841eed1f2bb951940a72f21ce20d969a
glebzok <mezentcev.g@outlook.com>
2020-10-21 10:45:46 +0300

    add: search by dataset feature

--- response.py
+++ response.py
@@ -2,6 +2,7 @@ from aticles_preprocesser import preprocess_text
 import numpy as np
 import pickle
 import os
+import re
 
 
 def get_best_matching_articles_idx(search_query,
@@ -24,6 +25,33 @@ def get_best_matching_articles_idx(search_query,
 
 def get_search_query_response(search_query, main_path='./papers_data', n=20):
     best_matching_articles_idx = get_best_matching_articles_idx(search_query)
+
+    with open(os.path.join(main_path, 'papers_index.pkl'), 'rb') as f:
+        papers_index = pickle.load(f)
+
+    best_matching_articles_names = [papers_index[paper_idx] for paper_idx in best_matching_articles_idx[:n]]
+
+    response = {'result': [{'name': paper_name.replace('_', ' '),
+                            'link': paper_name + '.pdf'} for paper_name in best_matching_articles_names]}
+
+    return response
+
+
+def get_best_matching_articles_by_dataset_idx(search_query, datasets_file='./papers_data/dataset_with_articles.pkl'):
+    with open(datasets_file, 'rb') as f:
+        datasets = pickle.load(f)
+
+    search_datasets = search_query[11:-1].split(',')
+    datasets = list(set([datasets[search_dataset] for search_dataset in search_datasets
+                         if search_dataset in datasets.keys()]))
+    return datasets
+
+
+def get_search_query_response(search_query, main_path='./papers_data', n=20):
+    if re.match('^data-eval:".*"$', search_query):
+        best_matching_articles_idx = get_best_matching_articles_by_dataset_idx(search_query)
+    else:
+        best_matching_articles_idx = get_best_matching_articles_idx(search_query)
 
     with open(os.path.join(main_path, 'papers_index.pkl'), 'rb') as f:
         papers_index = pickle.load(f)
